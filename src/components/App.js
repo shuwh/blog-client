be6231cbba7815c7facd9024b4a1378e1ba76f39
Steve <shuwh317@gmail.com>
2020-12-20 23:13:37 -0800

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import react from "react";
+import Landing from "./Landing";
+function App() {
+  return (
+    <div className="container">
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Landing.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+const Landing = () => {
+  return (
+    <section className="center-align">
+      <h1>Blogster!</h1>
+      write private blogs
+    </section>
+  );
+};
+
+export default Landing;
